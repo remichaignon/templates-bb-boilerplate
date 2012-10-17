c5c6558362afa79ee285ab971f4146cd16e02954
Rémi <remi.chaignon@gmail.com>
2012-10-16 19:14:19 -0600

    Add LESS build process

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-#echo ""
-#echo "[BUILD] Building the stylesheet files..."
-#echo ""
+echo ""
+echo "[BUILD] Building the stylesheet files..."
+echo ""
+lessc app/styles/master.less > dist/YOUR_APP.css
 
 echo ""
 echo "[BUILD] Building the template files..."
