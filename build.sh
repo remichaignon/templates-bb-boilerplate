00f7cc614cf34dc622450d7d0bc9aad7b9d25066
Rémi <remi.chaignon@gmail.com>
2012-10-21 21:29:49 -0600

    Compile LESS on the client side on dev

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 echo ""
 echo "[BUILD] Building the stylesheet files..."
-echo ""
-lessc app/styles/master.less > dist/YOUR_APP.css
+cd app/styles
+lessc master.less > ../../dist/YOUR_APP.css
+cd ../..
 
 echo ""
 echo "[BUILD] Building the template files..."
 echo ""
 python tools/templates.py
+
+echo ""
+echo "[BUILD] Optimizing the javascript files..."
+node tools/r.js -o tools/build.js
--- optimize.sh
+++
@@ -1,3 +0,0 @@
-echo ""
-echo "[BUILD] Optimizing the javascript files..."
-node tools/r.js -o tools/build.js
